ef0a7f850e9f16baa470b7c215418298dcbb8344
강민규 <gangmingyu@gangmingyuui-MacBookAir.local>
2021-05-31 23:57:40 +0900

    take 시리즈, throttle 알아보기

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -1,4 +1,4 @@
-import { all, call, fork, put, take } from 'redux-saga/effects';
+import { all, call, delay, fork, put, take, takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
 
 //3
@@ -12,7 +12,9 @@ function loginAPI(data, a, b, c) {
 function* logIn() {
   try {
     //명시되어있지 않아도 login이라는 함수에 action 객체가 넘어온 거임. call은 특이하게, 인자를 펼처서 보내줌. 첫번째 자리가 함수고, 나머지 자리는 매개변수
-    const result = yield call(loginAPI, action.data, a, b, c); //loginAPI의 결과값을 받아서 이펙트 앞에서 call, then(결과값)을 받기 위해서 call(동기) 써주셈!
+    //const result = yield call(loginAPI, action.data, a, b, c); //loginAPI의 결과값을 받아서 이펙트 앞에서 call, then(결과값)을 받기 위해서 call(동기) 써주셈!
+    yield delay(1000); //현재 서버가 없기 때문에 가짜로,
+
     yield put({
       //put은 디스패치라고 생각하면 된다. 액션을 실행
       type: 'LOG_IN_REQUEST',
@@ -34,15 +36,15 @@ function* logIn() {
 //1
 
 function* watchLogin() {
-  yield take('LOG_IN_REQUEST', logIn); //로그인이라는 액션이 실행될 때까지 기다리겠다. 액션이 실행되면 login함수 실행, 이벤트 리스너 같은 역할
+  yield takeEvery('LOG_IN_REQUEST', logIn); //로그인이라는 액션이 실행될 때까지 기다리겠다. 액션이 실행되면 login함수 실행, 이벤트 리스너 같은 역할
 }
 
 function* watchLogOut() {
-  yield take('LOG_OUT_REQUEST');
-}
+  yield takeEvery('LOG_OUT_REQUEST'); //take의 치명적인 단점은 1회용.. 1번 밖에 안 받음.. 해결은. while로 true로 감싸거나, 아니면 takeEvery.
+} //takeEvery 에서 마지막 것만 실행시키는 게 takeLatest(보통은 이거), 첫번쨰 것만 받고싶으면 takeleading
 
 function* watchAddPost() {
-  yield take('ADD_POST_REQUEST');
+  yield takeEvery('ADD_POST_REQUEST');
 }
 
 //2
